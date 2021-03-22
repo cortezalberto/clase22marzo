db964f39eff96393acc49d16e6a66415984c5af4
cortezalberto <cortezalberto@gmail.com>
2021-03-22 18:42:40 -0300

    Segundo commit

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const app = express()
 const port = 3000
 const path = require('path');
 app.use(express.static('public'));
-
+console.log('alberto')
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/indexflex.html'));
+    res.sendFile(path.join(__dirname, './views/tuflex.html'));
 });
 
 app.listen(port, () => {
